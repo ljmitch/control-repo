974d4a8bbb7f85d770ca5ad2a1887188d19d705d
Unknown <ljmitch@gmail.com>
2018-05-10 14:40:12 +0000

    Windows fied admins.pp

--- modules/system_users/manifests/admins.pp
+++ modules/system_users/manifests/admins.pp
@@ -1,18 +1,22 @@
 class system_users::admins {
 
-  package { 'csh':
-    ensure  => latest,
-  }
-
   group { 'staff':
     ensure  => present,
   }
 
-  user { 'admin':
-    ensure  => present,
-    group   => 'staff',
-    shell   => '/bin/csh',
-    require => Package['csh'],
-  }
+  if $facts['kernel'] == 'windows' {
+    user { 'admin':
+      group   => 'staff',
 
+    }
+  } else {
+    package { 'csh':
+      ensure  => latest,
+    }
+    user { 'admin':
+      group   => 'staff',
+      shell   => '/bin/csh',
+      require => Package['csh'],
+      }
+  }
 }

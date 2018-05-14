9fae33690f69de98243b98eebef7521b1c9b358f
Unknown <ljmitch@gmail.com>
2018-05-14 08:32:29 +0000

    Puppetfile with DSC and reboot

--- modules/nginxformasses/manifests/init.pp
+++ modules/nginxformasses/manifests/init.pp
@@ -1,17 +1,48 @@
-class system nginx::nginx {
+class nginxformasses (
+  String $docroot    = "/var/www",
+  String $portnumber = "80",
+) {
 
-    if $facts['kernel'] == 'windows' {
-      package { 'nginx-service':
-        ensure  => latest,
-        use_default_location: false
-        www_root: 'C:\ProgramData\nginx\html'
-      }
-    else {
-      package { 'nginx':
-        ensure  => latest,
-        use_default_location: false
-        www_root: ''/var/www'
-      }
+  if $facts['kernel'] =~ /[Ll]inux/ {
+    package { 'nginx':
+      ensure => latest,
+    }
+
+    file { '/etc/nginx/sites-available/default':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
+      content => template("${module_name}/default.erb"),
+      require => Package['nginx'],
+      notify  => Service['nginx'],
+    }
+
+    service { 'nginx':
+      ensure  => running,
+      require => Package['nginx'],
+    }
+
+    file { $docroot:
+      owner   => 'www-data',
+      group   => 'www-data',
+      mode    => '0755',
+      recurse => true,
+      require => Package['nginx'],
+    }
+
+    file { "${docroot}/index.html":
+      owner   => 'www-data',
+      group   => 'www-data',
+      mode    => '0755',
+      source  => "puppet:///modules/${module_name}/index.html",
+      require => File["${docroot}"],
     }
   }
+
+  else {
+    include chocolatey
+
+    package { 'nginx':
+      provider => 'chocolatey',
+    }
 }
